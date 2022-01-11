abeda3200345064120a11702f4503ea8530f976a
Artur Chirkun <death.fire1@ynadex.ru>
2022-01-11 17:38:59 +0300

    change timastamp

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 class EthereumExplorerService {
   arrayOfTimestamp = [
-    1578638524, 1641824468, 1641727268, 1641640868, 1641648068, 1641644468,
-    1641730868, 1641745268, 1641745868, 1641846668,
+    1641814501, 1641728101, 1641641701, 1641555301, 1641468901, 1641382501,
+    1641296101, 1641209701, 1641123301, 1641036901,
   ];
 
   API_KEY = `3FN3Y4IQDQWSK46ZJQS24QCQ5JF8YA1BU1`;
@@ -40,6 +40,7 @@ class EthereumExplorerService {
         const block = await this.getBlockByNumber(blockNumber);
         blocks.push(block);
         counter++;
+        console.log(block)
       } else {
         return;
       }
