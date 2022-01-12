465f120dc706ddc549f6f8b6e3152198bf587541
Artur Chirkun <death.fire1@ynadex.ru>
2022-01-12 17:52:34 +0300

    add function to save  block to your list

--- src/pages/details/details.js
+++ src/pages/details/details.js
@@ -5,9 +5,9 @@ import Overview from "../../components/overview/overview";
 
 const DetailsPage = ({ block }) => {
   return (
-    <div className="flex flex-col items-center justify-center w-8/12">
+    <div className="flex flex-col items-center justify-center w-5/12">
       <div className="flex border rounded-t-md w-full">
-        <div className="mr-1 p-3 hover:text-sky-500 hover:cursor-pointer border-2 border-transparent hover:border-b-sky-500 active:border-b-sky-500 ">
+        <div className="mr-1 p-3 text-sky-500 hover:cursor-pointer border-2 border-transparent  border-b-sky-500 ">
           Overview
         </div>
         <div className="pmr-1 p-3 hover:text-sky-500 hover:cursor-pointer border-2 border-transparent hover:border-b-sky-500 active:border-b-sky-500 ">
@@ -15,7 +15,7 @@ const DetailsPage = ({ block }) => {
         </div>
       </div>
       <Overview block={block} />
-      <div className=" mt-4 font-bold">
+      <div className=" mt-4 font-bold text-2xl hover:text-sky-500">
         <Link to="/"> Back </Link>
       </div>
     </div>
